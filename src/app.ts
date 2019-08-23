4b1535f3e29d98370660693321f23426895c63aa
georgylobko <mr.georgy93@gmail.com>
2019-08-24 00:25:20 +0300

    added bodyparser and koa-router

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-import Koa from "koa";
+import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
+import Router from 'koa-router';
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
-    ctx.body = "Hello World";
+router.get('/', (ctx, next) => {
+    ctx.body = { qwe: 'rty' };
 });
+
+router.get('/123', (ctx, next) => {
+    ctx.body = { qwe: 'rtyqqq' };
+});
+
+app.use(bodyParser({
+    jsonLimit: '56kb'
+}));
+
+app.use(router.routes()).use(router.allowedMethods());
 
 export default app;
