ce7af3748dbb65ac96a555fd6315d2375366fe22
georgylobko <mr.georgy93@gmail.com>
2019-08-25 01:04:32 +0300

    add config.js

---
+++ config/default.ts
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  server: {
+    host: 'http://localhost',
+    port: 3000,
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
 import app from "./app";
+import config from 'config';
 
-const server = app.listen(3000);
+const port = config.get('server.port');
+
+const server = app.listen(port, () => {
+    console.log(`Server up and running on port ${port}`);
+});
 
 export default server;
